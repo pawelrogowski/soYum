7326f2e3aa57c92f86ec2b67e09e822fd826acfb
Feiron <pawel.rogowski.praca@gmail.com>
2023-08-17 13:14:16 +0200

    feat/added theme switching button

--- src/components/ThemeToggle/ThemeToggle.jsx
+++
@@ -1,5 +0,0 @@
-import { StyledButton } from "./ThemeToggle.styled";
-
-export const ThemeToggle = (onClick) => {
-	return <StyledButton onClick={onClick}>ThemeToggle</StyledButton>;
-};
--- src/components/ThemeToggle/ThemeToggle.styled.js
+++ src/components/ThemeToggle/ThemeToggle.styled.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+
 export const StyledButton = styled.button`
 	background-color: ${(props) => props.theme.backgroundColor};
+	color: ${(props) => props.theme.primaryColor};
 `;
