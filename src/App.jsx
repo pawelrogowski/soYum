3a0b388f0bc37b0cd54dc4c929f28afc9410f04e
Feiron <pawel.rogowski.praca@gmail.com>
2023-08-17 13:15:44 +0200

    feat/implemented global context for theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
+import ThemeContext from "./contexts/ThemeContext";
 import { ThemeProvider } from "styled-components";
-import { useTheme } from "./hooks/useTheme";
-import styled from "styled-components";
-
-const ToggleButton = styled.button`
-	background-color: ${(props) => props.theme.backgroundColor};
-`;
+import { useContext } from "react";
+import { Container } from "./components/Container/Container";
+import { lightTheme, darkTheme, GlobalStyles } from "./styles/themes";
+import { ThemeSwitch } from "./components/ThemeToggle/ThemeSwitch";
 
 export const App = () => {
-	const { theme, toggleTheme } = useTheme();
+	const { theme } = useContext(ThemeContext);
 
 	return (
-		<ThemeProvider theme={theme}>
-			<div>
-				<ToggleButton onClick={toggleTheme}>Toggle Theme</ToggleButton>
-			</div>
+		<ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+			<GlobalStyles />
+			<Container>
+				<ThemeSwitch />
+			</Container>
 		</ThemeProvider>
 	);
 };
