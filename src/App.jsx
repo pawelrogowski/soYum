24c28c41357eb41af660d6f19af8db3f7f480638
feiron <pawel.rogowski.praca@gmail.com>
2023-09-05 13:21:08 +0200

    removed redundant import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "styled-components";
 import { useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { lightTheme, darkTheme, GlobalStyles } from "./styles/themes";
-import { ThemeSwitch } from "./components/ThemeToggle/ThemeSwitch";
 import { RegistrationPage } from "./pages/RegistrationPage";
 
 export const App = () => {
