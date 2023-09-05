6bc511d5d05166097ce11302389cfea26d288cd1
feiron <pawel.rogowski.praca@gmail.com>
2023-09-05 13:06:05 +0200

    updated styling

--- src/components/Icon/Icon.styled.js
+++ src/components/Icon/Icon.styled.js
@@ -1,17 +1,5 @@
-import styled, { css } from "styled-components";
+import { styled } from "styled-components";
 
 export const StyledSvg = styled.svg`
-  ${({ $icon }) =>
-    $icon === "404"
-      ? css`
-          fill: none;
-          width: 498px;
-          height: 300px;
-        `
-      : $icon === "hero-login-register"
-      ? css`
-          width: 285px;
-          height: 250px;
-        `
-      : null}
+  fill: none;
 `;
