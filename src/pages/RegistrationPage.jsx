9f8544f522d7e9be2c1c726d2e7cf7e6462f7d42
feiron <pawel.rogowski.praca@gmail.com>
2023-09-09 13:13:57 +0200

    updated hero icon logic

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -4,22 +4,21 @@ import { RegistrationForm } from "../components/Forms/RegistrationForm/Registrat
 import { ReactComponent as HeroIconMobile } from "../assets/icons/auth-hero/heroMobile.svg";
 import { ReactComponent as HeroIconTablet } from "../assets/icons/auth-hero/heroTablet.svg";
 import { ReactComponent as HeroIconDesktop } from "../assets/icons/auth-hero/heroDesktop.svg";
-
 import { StyledDiv } from "./RegistrationPage.styled";
 
 export const RegistrationPage = () => {
   const isTablet = useMediaQuery({ minWidth: breakpoints.tablet });
   const isDesktop = useMediaQuery({ minWidth: breakpoints.desktop });
 
+  const HeroIcon = isDesktop
+    ? HeroIconDesktop
+    : isTablet
+    ? HeroIconTablet
+    : HeroIconMobile;
+
   return (
     <StyledDiv>
-      {isDesktop ? (
-        <HeroIconDesktop />
-      ) : isTablet ? (
-        <HeroIconTablet />
-      ) : (
-        <HeroIconMobile />
-      )}
+      <HeroIcon />
       <RegistrationForm />
     </StyledDiv>
   );
