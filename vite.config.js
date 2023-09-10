ae7101c075873b13e85ce9db2ea5476f3c430b72
feiron <pawel.rogowski.praca@gmail.com>
2023-09-10 19:48:57 +0200

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
-import ViteCompressionPlugin from "vite-plugin-compression";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      template: "sunburst",
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-      filename: "bundle-size.html",
-    }),
-    ViteCompressionPlugin({
-      algorithm: "brotliCompress",
-    }),
-  ],
+  plugins: [react()],
 });
