78901775b6cda420ec63e200187e98fe92896942
Feiron <pawel.rogowski.praca@gmail.com>
2023-09-13 23:28:34 +0200

    code cleaning

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,16 +1,6 @@
-import styled, { css } from "styled-components";
-
-import { breakpoint } from "../../styles/mixins";
+import styled from "styled-components";
 
 export const StyledDiv = styled.div`
   padding: 0 2rem;
   min-width: 3.75rem;
-
-  ${({ $variant }) =>
-    $variant === "Registration"
-      ? css`=
-        `
-      : null}/* ${breakpoint.tablet(css`
-    padding: 4.4rem 5rem;
-  `)}; */
 `;
--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -3,6 +3,7 @@ import { Icon } from "../../components/Icon/Icon";
 import { useMediaQuery } from "react-responsive";
 import { breakpoints } from "../../styles/themes";
 import { PageContent } from "./AuthLayout.styled";
+
 export const AuthLayout = () => {
   const isDesktop = useMediaQuery({ minWidth: breakpoints.desktop });
   const isTablet = useMediaQuery({ minWidth: breakpoints.tablet });
