919a6250f0331c386c1778f4bccbf8477c6d1184
Feiron <pawel.rogowski.praca@gmail.com>
2023-09-15 01:27:15 +0200

    refactor/updated container styles

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,16 +1,11 @@
 import { StyledDiv } from "./Container.styled";
 import PropTypes from "prop-types";
 
-export const Container = ({ children, variant, as }) => {
-  return (
-    <StyledDiv as={as} $variant={variant}>
-      {children}
-    </StyledDiv>
-  );
+export const Container = ({ children, as }) => {
+  return <StyledDiv as={as}>{children}</StyledDiv>;
 };
 
 Container.propTypes = {
   children: PropTypes.node,
-  variant: PropTypes.string,
   as: PropTypes.string,
 };
