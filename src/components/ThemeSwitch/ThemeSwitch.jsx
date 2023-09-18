aa20769bf36b889ff17a09272b8e153d2fc6f26a
Feiron <pawel.rogowski.praca@gmail.com>
2023-09-18 23:18:31 +0200

    feat/pinned redux store to theme

--- src/components/ThemeSwitch/ThemeSwitch.jsx
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -1,14 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { StyledLabel } from "./ThemeSwitch.styled";
+import { changeTheme } from "../../redux/slices/globalSlice";
 
 export const ThemeSwitch = () => {
   const { toggleTheme } = useContext(ThemeContext);
+  const dispatch = useDispatch();
+  const isDarkTheme = useSelector((state) => state.global.isDarkTheme);
+  const [isChecked, setIsChecked] = useState(isDarkTheme);
+
+  const handleThemeChange = () => {
+    dispatch(changeTheme(!isDarkTheme));
+    toggleTheme();
+  };
+
+  useEffect(() => {
+    setIsChecked(isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <StyledLabel>
-      <input type="checkbox" onChange={toggleTheme} />
-      <span className="slider"></span>
+      <input
+        type="checkbox"
+        onChange={handleThemeChange}
+        defaultChecked={isChecked}
+      />
+      <span></span>
     </StyledLabel>
   );
 };
