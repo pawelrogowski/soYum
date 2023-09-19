0c2e0c80b13a425ce0f5596d2b635cfa7ddb6473
Feiron <pawel.rogowski.praca@gmail.com>
2023-09-19 11:43:35 +0200

    feat: avatar styles

---
+++ src/components/UserAvatar/UserAvatar.styled.js
@@ -0,0 +1,57 @@
+import { styled } from "styled-components";
+import { breakpoint, flexContainer, font } from "../../utils/mixins";
+
+export const StyledDiv = styled.div`
+  height: 100%;
+  margin-right: 2.4rem;
+  overflow: hidden;
+  ${flexContainer({
+    direction: "row",
+    justify: "flex-start",
+    align: "center",
+    wrap: "nowrap",
+    gap: "1.4rem",
+  })}
+
+  &:hover,
+  &:focus,
+  &:focus-within {
+    cursor: pointer;
+    img {
+      border: 0.1rem solid ${({ theme }) => theme.userAvatar.borderHover};
+    }
+    span {
+      color: ${({ theme }) => theme.userAvatar.fontHover};
+    }
+  }
+  ${breakpoint.tablet`
+    margin-right: 5rem;
+  `}
+
+  > picture > img {
+    width: 3.4rem;
+    height: 3.4rem;
+    border-radius: 50%;
+    background-color: lightgrey;
+    transition: border-color 250ms;
+    ${breakpoint.tablet`
+        width: 4.4rem;
+        height: 4.4rem;
+      `}
+  }
+
+  > span {
+    transition: color 250ms;
+    ${font({
+      family: "Poppins",
+      color: ({ theme }) => theme.userAvatar.fontIdle,
+      size: "1.2rem",
+      weight: "600",
+      height: "20.4rem",
+    })}
+    ${breakpoint.tablet`
+          font-size: 1.4rem;
+          line-height: 23.8rem;
+      `}
+  }
+`;
