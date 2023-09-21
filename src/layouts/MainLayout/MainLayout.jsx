57686a89b1499f6164773a6a494db567aaed230e
Feiron <pawel.rogowski.praca@gmail.com>
2023-09-21 20:13:00 +0200

    🚀 feat: added footer to main layout

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -4,8 +4,18 @@ import { Outlet } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { NavigationMobile } from "../../components/NavigationMobile/NavigationMobile";
 import { useSelector } from "react-redux";
+import { useMediaQuery } from "react-responsive";
+import { breakpoints } from "../../styles/themes";
+import { FooterMobile } from "../../components/Footer/Mobile/FooterMobile";
+import { FooterTablet } from "../../components/Footer/Tablet/FooterTablet";
+import { FooterDesktop } from "../../components/Footer/Desktop/FooterDesktop";
 
 export const MainLayout = () => {
+  const isMobileSize = useMediaQuery({ maxWidth: breakpoints.mobileMax });
+  const isTabletSize = useMediaQuery({
+    minWidth: breakpoints.tablet,
+    maxWidth: breakpoints.tabletMax,
+  });
   const isMobileMenuOpen = useSelector(
     (state) => state.global.isMobileMenuOpen
   );
@@ -18,6 +28,13 @@ export const MainLayout = () => {
       <Container>
         <Outlet />
       </Container>
+      {isMobileSize ? (
+        <FooterMobile />
+      ) : isTabletSize ? (
+        <FooterTablet />
+      ) : (
+        <FooterDesktop />
+      )}
     </>
   );
 };
