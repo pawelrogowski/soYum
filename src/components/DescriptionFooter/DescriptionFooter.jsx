8f1312374701ff42b138a825a577038dad0b644a
Feiron <pawel.rogowski.praca@gmail.com>
2023-09-21 20:13:18 +0200

    🚀 feat: added description element

---
+++ src/components/DescriptionFooter/DescriptionFooter.jsx
@@ -0,0 +1,12 @@
+import { StyledUl } from "./DescriptionFooter.styled";
+
+export const DescriptionFooter = () => {
+  return (
+    <StyledUl>
+      <li>Database of recipes that can be replenished</li>
+      <li>Flexible search for desired and unwanted ingredients</li>
+      <li>Ability to add your own recipes with photos</li>
+      <li>Convenient and easy to use</li>
+    </StyledUl>
+  );
+};
