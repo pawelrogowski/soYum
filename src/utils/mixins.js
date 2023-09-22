9a85c323b502e578b0626666006e5ef28a1d48b8
Feiron <pawel.rogowski.praca@gmail.com>
2023-09-23 01:57:51 +0200

    removed unused mixins

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,56 +1,5 @@
 import { css } from "styled-components";
 
-export const font = ({
-  color = "inherit",
-  size = "inherit",
-  family = "inherit",
-  height = "inherit",
-  spacing = "normal",
-  weight = "normal",
-  style = "normal",
-}) => css`
-  color: ${color};
-  font-size: ${size};
-  font-family: ${family};
-  line-height: ${height};
-  letter-spacing: ${spacing};
-  font-weight: ${weight};
-  font-style: ${style};
-`;
-
-export const flexContainer = ({
-  direction = "row",
-  justify = "flex-start",
-  align = "stretch",
-  wrap = "nowrap",
-  gap = "0",
-} = {}) => css`
-  display: flex;
-  flex-direction: ${direction};
-  justify-content: ${justify};
-  align-items: ${align};
-  flex-wrap: ${wrap};
-  gap: ${gap};
-`;
-
-export const breakpoint = {
-  tablet: (...styles) => {
-    return css`
-      @media (min-width: ${(props) => props.theme.breakpoints.tablet}) {
-        ${css(...styles)}
-      }
-    `;
-  },
-
-  desktop: (...styles) => {
-    return css`
-      @media (min-width: ${(props) => props.theme.breakpoints.desktop}) {
-        ${css(...styles)}
-      }
-    `;
-  },
-};
-
 export const responsiveBackgroundImage = (image1x, image2x, image3x) => css`
   background-image: url(${image1x});
 
