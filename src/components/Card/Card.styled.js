0fdfcb2979e96214b6c2ab0cd74c552a35e82aca
Feiron <pawel.rogowski.praca>
2023-09-29 19:27:39 +0200

    ✨ feat: implemented food card component

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const StyledFigure = styled.figure``;
+export const StyledFigure = styled.figure`
+  position: relative;
+  display: flex;
+  border-radius: 12px;
+  overflow: hidden;
+  figcaption {
+    position: absolute;
+    bottom: 2.6rem;
+    left: 1.8rem;
+
+    background-color: #fff;
+
+    border-radius: 4px;
+    padding: 1.6rem;
+    width: calc(100% - 3.2rem);
+
+    font-size: 1.6rem;
+    font-weight: 500;
+    line-height: 1.25;
+    letter-spacing: -0.024rem;
+  }
+  picture {
+    width: 100%;
+  }
+  img {
+    width: 100%;
+  }
+`;
