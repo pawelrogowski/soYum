3cf62832c26f67d9dca231893f4853cc9c84c050
Feiron <pawel.rogowski.praca>
2023-09-30 16:08:25 +0200

    bugfix: headings color

--- src/components/Heading/Heading.styled.js
+++ src/components/Heading/Heading.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledHeading = styled.h1`
-  --color-text: ${({ theme }) => theme.heading.color}
+  --color-text: ${({ theme }) => theme.heading.color};
   color: var(--color-text);
   font-size: 2.8rem;
   font-weight: 600;
