a5ef72b0d84810939b2f6b79d118faadd8728a43
Feiron <pawel.rogowski.praca>
2023-10-02 10:29:39 +0000

    Oct 2, 2023, 12:29 PM

--- src/components/UserAvatar/UserAvatar.jsx
+++ src/components/UserAvatar/UserAvatar.jsx
@@ -2,15 +2,28 @@ import PropTypes from "prop-types";
 import { shortenString } from "../../utils/stringManipulation";
 import { StyledDiv } from "./UserAvatar.styled";
 import { EditProfilePopup } from "../EditProfilePopup/EditProfilePopup";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleUserEditMenu } from "../../redux/slices/modalSlice";
+
 export function UserAvatar({ image, placeholder, name = "No Data" }) {
+  const dispatch = useDispatch();
+
+  const isEditProfilePopup = useSelector(
+    (state) => state.modal.isUserEditMenuOpen
+  );
+
+  const handleUserEditToggle = () => {
+    dispatch(toggleUserEditMenu());
+  };
+
   return (
-    <StyledDiv>
+    <StyledDiv onClick={handleUserEditToggle}>
       <picture>
         <source srcSet={image} type="image/jpeg" />
         <img src={placeholder} alt="user avatar" />
       </picture>
       <span>{shortenString(name, 10, "...")}</span>
-      <EditProfilePopup />
+      {isEditProfilePopup && <EditProfilePopup />}
     </StyledDiv>
   );
 }
