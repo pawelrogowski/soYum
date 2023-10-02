0b926b88f7a03f5edf597f3c350b0a59ee4e54ff
Feiron <pawel.rogowski.praca@gmail.com>
2023-10-02 13:20:27 +0000

    Oct 2, 2023, 3:20 PM

--- src/components/Forms/RegisterForm/RegisterForm.jsx
+++ src/components/Forms/RegisterForm/RegisterForm.jsx
@@ -14,7 +14,14 @@ export const RegistrationForm = () => {
   const warnIconMotion = {
     initial: { scale: 0.2 },
     animate: { scale: 1 },
-    exit: { scale: 0.2 },
+    exit: {
+      scale: 0.2,
+      transition: {
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      },
+    },
     transition: {
       type: "spring",
       stiffness: 260,
