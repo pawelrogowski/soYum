f5125666aac2ebbeea5a8561459656a5d72f1277
Feiron <pawel.rogowski.praca@gmail.com>
2023-10-02 21:24:38 +0200

    changed blur

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -27,7 +27,7 @@ export const StyledDiv = styled.main`
         bottom: -26.4rem;
         left: -10rem;
         rotate: -217deg;
-        filter: blur(0.45rem);
+        filter: blur(0.15rem);
         transform: scaleX(-1);
         z-index: -1;
         @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
