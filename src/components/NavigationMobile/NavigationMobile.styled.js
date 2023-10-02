659a99b66938da91561459fc7500656b0ac6a910
Feiron <pawel.rogowski.praca>
2023-10-02 10:32:07 +0000

    Oct 2, 2023, 12:32 PM

--- src/components/NavigationMobile/NavigationMobile.styled.js
+++ src/components/NavigationMobile/NavigationMobile.styled.js
@@ -10,7 +10,7 @@ export const StyledAside = styled(motion.aside)`
   --breakpoint-tablet: ${({ theme }) => theme.breakpoints.tablet};
 
   position: absolute;
-  z-index: 1;
+  z-index: 2;
   top: 0;
   left: 0;
   width: 100vw;
