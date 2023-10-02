d120939a99b1456221ea4232fa1b684fda0265e3
Feiron <pawel.rogowski.praca@gmail.com>
2023-10-02 11:35:28 +0000

    Oct 2, 2023, 1:35 PM

--- src/components/EditProfilePopup/EditProfilePopup.jsx
+++ src/components/EditProfilePopup/EditProfilePopup.jsx
@@ -1,10 +1,38 @@
 import { Icon } from "../Icon/Icon";
 import { StyledDiv } from "./EditProfilePopup.styled";
 import { Button } from "../Button/Button";
+import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { toggleUserEditMenu } from "../../redux/slices/modalSlice";
 
 export const EditProfilePopup = () => {
+  const dispatch = useDispatch();
+  const ref = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      dispatch(toggleUserEditMenu(false));
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Escape") {
+      dispatch(toggleUserEditMenu(false));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-    <StyledDiv>
+    <StyledDiv ref={ref}>
       <div>
         <span>Edit profile</span>
         <Icon icon="edit" />
--- src/components/UserAvatar/UserAvatar.jsx
+++ src/components/UserAvatar/UserAvatar.jsx
@@ -4,11 +4,9 @@ import { StyledDiv } from "./UserAvatar.styled";
 import { EditProfilePopup } from "../EditProfilePopup/EditProfilePopup";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleUserEditMenu } from "../../redux/slices/modalSlice";
-import { useEffect, useRef } from "react";
 
 export function UserAvatar({ image, placeholder, name = "No Data" }) {
   const dispatch = useDispatch();
-  const popupRef = useRef(null);
 
   const isEditProfilePopupOpen = useSelector(
     (state) => state.modal.isUserEditMenuOpen
@@ -18,28 +16,6 @@ export function UserAvatar({ image, placeholder, name = "No Data" }) {
     !isEditProfilePopupOpen && dispatch(toggleUserEditMenu(true));
   };
 
-  const handleClickOutside = (event) => {
-    if (popupRef.current && !popupRef.current.contains(event.target)) {
-      dispatch(toggleUserEditMenu(false));
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Escape") {
-      dispatch(toggleUserEditMenu(false));
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
   return (
     <StyledDiv onClick={handleOpenUserEdit}>
       <picture>
@@ -47,7 +23,7 @@ export function UserAvatar({ image, placeholder, name = "No Data" }) {
         <img src={placeholder} alt="user avatar" />
       </picture>
       <span>{shortenString(name, 10, "...")}</span>
-      {isEditProfilePopupOpen && <EditProfilePopup ref={popupRef} />}
+      {isEditProfilePopupOpen && <EditProfilePopup />}
     </StyledDiv>
   );
 }
