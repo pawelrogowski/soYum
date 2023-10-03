da3c168d7df03548bbeb24d137ff147c064deed6
Feiron <pawel.rogowski.praca@gmail.com>
2023-10-03 17:31:35 +0200

    🔀🎨refactor: styles

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,5 +1,5 @@
-import styled, { css } from "styled-components";
-import leaves from "../../assets/images/leaves@1x.webp";
+import styled from "styled-components";
+
 export const StyledDiv = styled.main`
   max-width: ${({ theme }) => theme.breakpoints.maxContent};
   min-width: ${({ theme }) => theme.breakpoints.minContent};
@@ -7,42 +7,4 @@ export const StyledDiv = styled.main`
   position: relative;
   margin: 0 auto;
   width: 100%;
-
-  ${(props) =>
-    props.as === "main" &&
-    css`
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding-bottom: 10rem;
-
-      @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
-        padding-bottom: 20rem;
-      }
-      > section:last-of-type {
-        margin-bottom: 3.2rem;
-      }
-      &:after {
-        content: "";
-        position: absolute;
-        background-image: url(${leaves});
-        background-repeat: no-repeat;
-        background-size: cover;
-        width: 2.95rem;
-        height: 46.8rem;
-        bottom: -26.4rem;
-        left: -10rem;
-        rotate: -217deg;
-        filter: blur(0.15rem);
-        transform: scaleX(-1);
-        z-index: -1;
-        @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
-          width: 33.5rem;
-          height: 51.7rem;
-          bottom: -29.9rem;
-          left: -10.1rem;
-          rotate: -217deg;
-        }
-      }
-    `}
 `;
