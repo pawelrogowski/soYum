d47add0b32687efb74221a031d3891ebd373307b
Feiron <pawel.rogowski.praca@gmail.com>
2023-10-18 01:41:07 +0200

    🔀 Refactored code

--- src/components/NavigationFooter/NavigationFooter.jsx
+++ src/components/NavigationFooter/NavigationFooter.jsx
@@ -5,7 +5,7 @@ import { StyledNav } from "./NavigationFooter.styled";
 export function NavigationFooter() {
   const navItems = [
     { path: "/ingredients", text: "Ingredients" },
-    { path: "/recipes", text: "Add recipes" },
+    { path: "/add", text: "Add recipes" },
     { path: "/myrecipes", text: "My recipes" },
     { path: "/favorites", text: "Favorites" },
     { path: "/shoppinglist", text: "Shopping List" },
