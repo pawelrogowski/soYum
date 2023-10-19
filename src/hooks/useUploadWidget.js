6bc56d300a2c31441dd50486a004936037725534
Feiron <pawel.rogowski.praca@gmail.com>
2023-10-19 18:27:19 +0200

    🚀feat: implemented new widget hook

---
+++ src/hooks/useUploadWidget.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+function useUploadWidget() {
+  useEffect(() => {
+    const scriptUrl = "https://widget.cloudinary.com/v2.0/global/all.js";
+
+    const existingScript = document.querySelector(`script[src="${scriptUrl}"]`);
+
+    if (!existingScript) {
+      const script = document.createElement("script");
+      script.src = scriptUrl;
+      script.async = true;
+      document.head.appendChild(script);
+    }
+
+    return () => {
+      document.querySelectorAll("iframe").forEach((iframe) => {
+        if (iframe.title === "Upload Widget") {
+          iframe.remove();
+        }
+      });
+    };
+  }, []);
+}
+
+export default useUploadWidget;
