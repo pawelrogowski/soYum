1dcafacc5753ff561376ceca42a73f42bfc8b1dd
Feiron <pawel.rogowski.praca@gmail.com>
2023-10-19 18:28:56 +0200

    📦 added eslint-config-prettier package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,14 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh", "simple-import-sort"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
   },
