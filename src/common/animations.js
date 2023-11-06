50fa3017a0a0913b46532a2e1e0ee762a7d22091
feiron <pawel.rogowski.praca@gmail.com>
2023-11-07 00:13:14 +0100

    added animation object for start page

--- src/common/animations.js
+++ src/common/animations.js
@@ -124,5 +124,5 @@ export const startPageMotion = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 },
-  transition: { duration: 0.3 },
+  transition: { duration: 0.1 },
 };
