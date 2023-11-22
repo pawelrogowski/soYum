19a67fbcaf7c5b99582d1e7da4e292739bdf385b
feiron <pawel.rogowski.praca@gmail.com>
2023-11-22 03:11:06 +0100

    moved App to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 
-import { AppRoutes } from "./routes/Routes";
-import { GlobalStyles } from "./styles/globalStyles";
-import { darkTheme, lightTheme } from "./styles/themes";
+import { AppRoutes } from "../routes/Routes";
+import { GlobalStyles } from "../styles/globalStyles";
+import { darkTheme, lightTheme } from "../styles/themes";
 
 export const App = () => {
   const isDarkTheme = useSelector((state) => state.global.isDarkTheme);
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
-import { App } from "./App.jsx";
+import { App } from "./components/App.jsx";
 import { persistor, store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
