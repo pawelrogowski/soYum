be8feadc35a74cfda39c9f3566d4c706c11420dc
feiron <pawel.rogowski.praca@gmail.com>
2023-11-27 04:28:54 +0100

    changed title hook

--- src/hooks/usePageTitle.js
+++ src/hooks/usePageTitle.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 const baseTitle = "SoYummy";
-const separator = "-";
+const separator = "•";
 
 export const usePageTitle = (subTitle) => {
   useEffect(() => {
